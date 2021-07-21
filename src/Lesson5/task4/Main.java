e4d96cde008addd649172d932c563812995d683b
Bodun <bbigun2000@gmail.com>
2021-07-21 21:50:51 +0300

    Dev

---
+++ src/Lesson5/task4/Main.java
@@ -0,0 +1,22 @@
+package Lesson5.task4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList <String> words = new ArrayList<>();
+
+        words.add("Мама");
+        words.add("мила");
+        words.add("раму");
+
+        for (String s : words){
+            System.out.println(s);
+            for(int i = 2; i < words.size(); i++){
+                System.out.println("Іменно");
+            }
+        }
+    }
+}
